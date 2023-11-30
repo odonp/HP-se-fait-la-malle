d715f7a2711fc199bf9a30507dac74b953c5618f
CherryBlossom44 <151512201+CherryBlossom44@users.noreply.github.com>
2023-11-30 12:54:20 +0100

    Update projet hp.py
    ajout du programme pour le magasin de livre et l'interface homme machine. Problématique ca ne renvoi pas les billets rendu + si l'utilisateur ne répond pas a la question par oui il n'y a pas de boucle

--- projet hp.py
+++ projet hp.py
@@ -48,6 +48,39 @@ if entrée == "oui" or entrée == "OUI" or entrée == "Oui" :
             autre_magasin = input("Voulez vous terminez vos course par achetez une baguette ? ")
 else:
     entrée = input("Ëtes vous sur ?, vous entré dans le chemin de traverse, pret à acheter vos fournitures ? ")
+
+'''
+les deux version ensemble qui marche pour les livre 
+'''
+entrée = input("Bonjour sorcier/e, vous entrez dans le chemin de traverse, prêt à acheter vos fournitures ? ")
+if entrée == "oui" or entrée == "OUI" or entrée == "Oui" :
+    livre = input("vous entré Chez Fleury & Bott voulez vous achetez : Le Livre des sorts et enchantements, niveau 1 de Miranda Fauconnette et Histoire de la magie de Bathilda Tourdesac ? ")
+    if livre == "oui" or livre == "OUI" or livre == "Oui" : 
+        soustraction = 0
+        argent = int(input("Combien avez-vous payé ? "))
+        monnaie = [500,200,100,50,20,10,5,2,1]
+        monnaie.append(argent)
+        monnaie_trie = sorted(monnaie, reverse = True)
+        index = monnaie_trie.index(argent)
+        soustraction = argent - monnaie[index]
+        print(index)
+    else : 
+        livre = input("vous entré Chez Fleury & Bott voulez vous achetez : Le Livre des sorts et enchantements, niveau 1 de Miranda Fauconnette et Histoire de la magie de Bathilda Tourdesac ? ")
+        magasin = input("Voulez vous continuer vos achat et achetez une cape ? ")
+        if magasin == "oui" or magasin == "Oui" or magasin == "OUI" : 
+            autre_magasin = input("Voulez vous terminez vos course par achetez une baguette ? ")
+            if autre_magasin == "oui" or "Oui" or "OUI":
+                Ors = int(input("Combien vaut 1 Gallion ?"))
+                Gallion = Ors
+                Mornille = Ors / 17
+                Gallion = Mornille * 17
+                Noises = Mornille / 29 
+                Mornille = 29 * Noises
+
+        else : 
+            autre_magasin = input("Voulez vous terminez vos course par achetez une baguette ? ")
+else:
+    entrée = input("Ëtes vous sur ?, vous entré dans le chemin de traverse, pret à acheter vos fournitures ? ")
     
 
 
