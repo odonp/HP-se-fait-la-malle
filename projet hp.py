9fa55b7758f072ca8454b87f7e79194629f89385
zblou44 <151512087+zblou44@users.noreply.github.com>
2023-11-30 09:39:24 +0100

    Update projet hp.py
    on a réussi à afficher la monnaie rendue

--- projet hp.py
+++ projet hp.py
@@ -1,53 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 23 09:30:49 2023
-
-@author: loubna.zbitou
-"""
-
-argent = int(input("Combien avez-vous payé ?"))
-
-while argent > 0 :
-    if argent > 500 :
-        argent = argent - 500
-        print (500)
-    elif argent > 200 :
-        argent = argent - 200
-        print (200)
-    elif argent > 100 :
-        argent = argent - 100
-        print (100)
-    elif argent > 50 :
-        argent = argent - 50
-        print (50)
-    elif argent > 20 :
-        argent = argent - 20
-        print (20)
-    elif argent > 10 :
-        argent = argent - 10
-        print (10)
-    elif argent > 5 :
-        argent = argent - 5
-        print (5)
-    elif argent > 2 :
-        argent = argent - 2
-        print (2)
-    else :
-        argent = argent - 1
-        print (1)
-
-Deux idées pour continuer le projet :
-
-argent = int(input("Combien avez-vous payé ? "))
-monnaie = {'Billet' : 500,'Billet' : 200,'Billet' : 100,'Billet' : 50,'Billet' : 20,'Billet' : 10,'Billet': 5,'Pièce' : 2,'Pièce' : 1}
-monnaie.values(argent)
-
-
-prix = int(input("Quel est le prix à payer ? "))
-monnaie = [{'Billet' : '500'},{'Billet' : '200'},{'Billet' : '100'},{'Billet' : '50'},{'Billet' : '20'},{'Billet' : '10'},{'Billet': '5'},{'Pièce' : '2'},{'Pièce' : '1'}]
-monnaie.append(prix)    
-print(monnaie)
-
 # interface homme machine avec conversion de l'argent
 
 entrée = input("Bonjour sorcier/e, vous entrez dans le chemin de traverse, pret a acheter vos fourniture ?")
@@ -74,4 +24,26 @@ if entrée == "oui" :
 
 else:
     entrée = int(input("Ëtes vous sur ?, vous entré dans le chemin de traverse, pret a acheter vos fourniture ?"))
+
+
+
+
+"""
+Autres
+"""
+
+
+
+soustraction = 0
+
+
+argent = int(input("Combien avez-vous payé ? "))
+monnaie = [500,200,100,50,20,10,5,2,1]
+monnaie.append(argent)
+print(monnaie)
+monnaie_trie = sorted(monnaie, reverse = True)
+print(monnaie_trie)
+
+index = monnaie_trie.index(argent)
+soustraction = argent - monnaie[index]
     
