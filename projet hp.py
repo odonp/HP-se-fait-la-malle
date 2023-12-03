d1b8eac6b7d4b852cadb1e0911953642dd9037f2
odonp <150008368+odonp@users.noreply.github.com>
2023-12-03 22:07:43 +0100

    Update projet hp.py
    Ajout de la fonction de la partie en illimitée mais il faut encore l'améliorer

--- projet hp.py
+++ projet hp.py
@@ -1,3 +1,71 @@
+'''
+Harry Potter se fait la malle
+
+
+REGLES:
+
+
+
+
+AUTEURS:
+Valentine
+Loubna
+Odon
+
+LICENCE:
+
+
+
+VERSION:
+
+
+
+DATE DE DERNIERE REVISION:
+
+
+
+ADRESSE GITHUB: 
+
+
+'''
+
+# coding: utf-8
+
+#import des modules
+
+from random import randint
+
+
+#définition des constantes
+
+
+
+
+#définition des variables 
+argent_rendu = []
+
+#définitionn des fonctions dans l'ordre d'utlilisation
+
+def rendu_monnaie_illimitee(argent_a_rendre):
+    while argent_a_rendre > 0 :
+        monnaie = [500,200,100,50,20,10,5,2,1]
+        monnaie.append(argent_a_rendre)
+        monnaie.sort(reverse = True)
+        index = monnaie.index(argent_a_rendre)
+        argent_a_rendre = argent_a_rendre - monnaie[index + 1]
+        argent_rendu.append(monnaie[index+1])
+    return argent_rendu
+
+
+
+
+
+#DEBUT DU PROGRAMME PRINCIPAL
+
+
+
+
+
 """
 idée du prof
 """
@@ -23,7 +91,7 @@ def affichage(monnaie_a_rendre):
 notre programme qui marche 
 """
 
-
+'''
 entrée = input("Bonjour sorcier/e, vous entrez dans le chemin de traverse, prêt à acheter vos fournitures ? (répondez 'oui' ou 'non')")
 if entrée == "oui" or entrée == "OUI" or entrée == "Oui" :
     livre = input("vous entré Chez Fleury & Bott voulez vous achetez : Le Livre des sorts et enchantements, niveau 1 de Miranda Fauconnette et Histoire de la magie de Bathilda Tourdesac ? (répondez 'oui' ou 'non')")
@@ -55,3 +123,5 @@ if entrée == "oui" or entrée == "OUI" or entrée == "Oui" :
             autre_magasin = input("Voulez vous terminez vos course par achetez une baguette ? (répondez 'oui' ou 'non')")
 else:
     entrée = input("Ëtes vous sur ?, vous entré dans le chemin de traverse, pret à acheter vos fournitures ? (répondez 'oui' ou 'non')")
+    '''
+
