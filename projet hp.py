9da01abf00a34cbbe7690498da58df5eb327737d
zblou44 <151512087+zblou44@users.noreply.github.com>
2023-12-04 09:54:44 +0100

    Update projet hp.py
    add dico mais non fonctionnrl

--- projet hp.py
+++ projet hp.py
@@ -1,4 +1,4 @@
-'''
+"""
 Harry Potter se fait la malle
 
 
@@ -25,9 +25,7 @@ DATE DE DERNIERE REVISION:
 
 
 ADRESSE GITHUB: 
-
-
-'''
+"""
 
 # coding: utf-8
 
@@ -44,7 +42,7 @@ from random import randint
 #définition des variables 
 argent_rendu = []
 
-#définitionn des fonctions dans l'ordre d'utlilisation
+#définition des fonctions dans l'ordre d'utlilisation
 
 def rendu_monnaie_illimitee(argent_a_rendre):
     while argent_a_rendre > 0 :
@@ -58,7 +56,7 @@ def rendu_monnaie_illimitee(argent_a_rendre):
 
 
 
-
+"""
 
 #DEBUT DU PROGRAMME PRINCIPAL
 
@@ -67,11 +65,10 @@ def rendu_monnaie_illimitee(argent_a_rendre):
 
 
 """
-idée du prof
-"""
+
 
 '''
-
+idée du prof
 def rendu_fleury(a_rendre):
     monnaie = (500, 200, 100, 50, 20, 10, 5, 2, 1)
     rendu = []
@@ -88,27 +85,29 @@ def affichage(monnaie_a_rendre):
  
  
 """
+
+
 notre programme qui marche 
 """
-
-
 #On est obligé d'avoir les oui ? Ca alourdit vraiment et ça multiplie les erreurs
+dico = {500 : 0 ,200:0,100:0,50:0,20:0,10:0,5:0,2:0,1:0  }
 
-entrée = input("Bonjour sorcier/e, vous entrez dans le chemin de traverse, prêt à acheter vos fournitures ? (répondez 'oui' ou 'non') ")
+entrée = input("Bonjour sorcier/e, vous entrez dans le chemin de traverse, prêt à acheter vos fournitures ? (répondez 'oui' ou 'non')")
 if entrée == "oui" or entrée == "OUI" or entrée == "Oui" :
-    livre = input("vous entrez Chez Fleury & Bott voulez vous achetez : Le Livre des sorts et enchantements, niveau 1 de Miranda Fauconnette et Histoire de la magie de Bathilda Tourdesac ? (répondez 'oui' ou 'non') ")
+    livre = input("vous entrez Chez Fleury & Bott voulez vous achetez : Le Livre des sorts et enchantements, niveau 1 de Miranda Fauconnette et Histoire de la magie de Bathilda Tourdesac ? (répondez 'oui' ou 'non')")
     if livre == "oui" or livre == "OUI" or livre == "Oui" : 
-        argent = int(input("Combien doit-ont vous rendre ? (entrez un entier) "))
+        argent = int(input("Combien doit-ont vous rendre ? (entrez un entier)"))
         
         liste_rendu_monnaie = rendu_monnaie_illimitee(argent)
-        print(liste_rendu_monnaie)
+        
         for i in range(len(liste_rendu_monnaie)) :
+            if liste_rendu_monnaie[i] == dico[liste_rendu_monnaie[i]]:
+                dico[liste_rendu_monnaie[i]] += 1
             print(f"Je vous rend {liste_rendu_monnaie[i]}€ m.Potter")
+            print(dico[liste_rendu_monnaie[i]])
         if len(liste_rendu_monnaie)== 0 :
             print("Je vous rend 0€ M. Potter")
-
-
-
+            
 
 
 
@@ -150,4 +149,4 @@ if entrée == "oui" or entrée == "OUI" or entrée == "Oui" :
             autre_magasin = input("Voulez vous terminez vos course par achetez une baguette ? (répondez 'oui' ou 'non')")
 else:
     entrée = input("Ëtes vous sur ?, vous entré dans le chemin de traverse, pret à acheter vos fournitures ? (répondez 'oui' ou 'non')")
-    '''
+'''
