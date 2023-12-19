a127199b995aaa6fc5b8296eb39ce9f734def1fe
odonp <150008368+odonp@users.noreply.github.com>
2023-12-19 21:55:33 +0100

    Update projet hp.py
    fini Le 3)
    Il faut maintenant peaufiner le tout, faire le 2) et trouver un moyen de tout reset à la fin du programme

--- projet hp.py
+++ projet hp.py
@@ -25,7 +25,12 @@ DATE DE DERNIERE REVISION:
 
 
 ADRESSE GITHUB: 
+https://github.com/odonp/HP-se-fait-la-malle
+
 """
+
+#Moyen à la fin du programe de tout quitter ("si vous voulez partir, tapez ... " et ca quitte)
+
 
 # coding: utf-8
 
@@ -41,6 +46,7 @@ from random import randint
 
 #définition des variables 
 argent_rendu = []
+
 dico_monnaie_illimitée = {\
   500 : 0 ,
   200:0 ,
@@ -53,7 +59,11 @@ dico_monnaie_illimitée = {\
   1:0 }
 
 
+
 #définition des fonctions dans l'ordre d'utlilisation
+
+
+
 
 def menu():
     reponse = input("Bonjour sorcier/e. Tu es sur le Chemin de traverse.\nSi tu souhaites aller chez Fleury et Bott, librairie de sorcier, tape 1 \nSi tu souhaites aller chez Madame Guipure, magasin de prêt à porter pour mages et sorcier, tape 2 \nSi tu souhaites aller chez Ollivander, fabricant de baguettes magiques, tape 3 \nOù souhaites-tu aller ? ")
@@ -68,16 +78,17 @@ def menu():
             print(f"Rendu monnaie pour {elements}€")
             chez_fleury_et_bott(elements)
             print("\n")
-
-        
-        
-        
     
     elif reponse == '2' :
-        djdsj
+        Guipur()
+    
     #Si on répond n'importe quoi ca fait quand meme ollivander
+    elif reponse == '3':
+        print(Ollivander())
     else :
-        Chez_Ollivander()
+        print("Merci d'entrer un nombre entier, compris entre 1 et 3 et correspondant au bon magasion du chemin de traverse")
+        #Moyen de relancer le programme au menu 
+
 
 def chez_fleury_et_bott(argent_a_rendre):
     
@@ -104,6 +115,10 @@ def chez_fleury_et_bott(argent_a_rendre):
         argent_a_rendre = argent_a_rendre - monnaie[index + 1]
         argent_rendu.append(monnaie[index+1])
     
+    
+    if argent_a_rendre2 == 0 :
+        print("Le vendeur n'a rien à vous rendre")
+
     for i in dico_monnaie_illimitée.keys():
             while argent_a_rendre2 >= i:
                 dico_monnaie_illimitée[i] += 1
@@ -111,21 +126,31 @@ def chez_fleury_et_bott(argent_a_rendre):
     for i in dico_monnaie_illimitée.keys():
         if dico_monnaie_illimitée[i] > 0 :
             print(f"Le vendeur vous rend {dico_monnaie_illimitée[i]} coupure.s de {i} €")
+   
 
-a_rendre = []
-gallions = int(input("Combien de gallions ? "))
-mornilles = int(input("Combien de mornilles ? "))
-noises = int(input("Combien de noises ? "))
-somme_en_noises = (gallions * 17 * 29) + (mornilles * 29) + noises
-gallions_a_rendre = somme_en_noises // (17*29)
-a_rendre.append(gallions_a_rendre)
-somme_en_noises -= gallions_a_rendre * (17*29)
-mornilles_a_rendre = somme_en_noises // 29
-a_rendre.append(mornilles_a_rendre)
-somme_en_noises -= mornilles_a_rendre * 29
-noises_a_rendre = somme_en_noises
-a_rendre.append(noises_a_rendre)
-print(a_rendre)
+
+def Ollivander ():
+    gallions = 0
+    mornilles = 0
+    noises = 0
+    a_rendre = []
+    paroles = ["Gallions","Mornilles", "Noises"]
+    gallions = int(input("Combien de gallions dois-je rendre ? (Entrez une valeur)"))
+    mornilles = int(input("Combien de mornilles dois-je rendre ? (Entrez une valeur) "))
+    noises = int(input("Combien de noises dois-je rendre ? (Entrez une valeur) "))
+    somme_en_noises = (gallions * 17 * 29) + (mornilles * 29) + noises
+    gallions_a_rendre = somme_en_noises // (17*29)
+    a_rendre.append(gallions_a_rendre)
+    somme_en_noises -= gallions_a_rendre * (17*29)
+    mornilles_a_rendre = somme_en_noises // 29
+    a_rendre.append(mornilles_a_rendre)
+    somme_en_noises -= mornilles_a_rendre * 29
+    noises_a_rendre = somme_en_noises
+    a_rendre.append(noises_a_rendre)
+    
+    for i in range (3):
+        print(f"Je vous rends {a_rendre[i]} {paroles[i]}")
+
 
 '''
 def affichage_resultat() :
@@ -158,3 +183,9 @@ def affichage_resultat() :
             print(f"le vendeur vous rend {dico_monnaie_illimitée[i]} coupure.s de {i}")
 '''
 
+
+"""
+PROGRAMME PRINCIPAL
+"""
+#Notre programme principal, constitué de fonctions
+menu()
