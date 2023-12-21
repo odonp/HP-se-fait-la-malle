00c2162a888bd515b3f486a08f052e8420887eb8
odonp <150008368+odonp@users.noreply.github.com>
2023-12-22 00:52:30 +0100

    Fonction affichage et finition du 1 projet hp.py
    Reste le pseudo code, le reset (pas important), et le 2) avec possibilité de donner toute la caisse

--- projet hp.py
+++ projet hp.py
@@ -5,8 +5,6 @@ Harry Potter se fait la malle
 REGLES:
 
 
-
-
 AUTEURS:
 Valentine
 Loubna
@@ -66,21 +64,29 @@ dico_monnaie_illimitée = {\
 
 
 def menu():
-    reponse = input("Bonjour sorcier/e. Tu es sur le Chemin de traverse.\nSi tu souhaites aller chez Fleury et Bott, librairie de sorcier, tape 1 \nSi tu souhaites aller chez Madame Guipure, magasin de prêt à porter pour mages et sorcier, tape 2 \nSi tu souhaites aller chez Ollivander, fabricant de baguettes magiques, tape 3 \nOù souhaites-tu aller ? ")
+    '''
+    Régie tout le programme en faisant appel aux fonctions
+    Entré : 
+    Sortie 
+    '''
+    reponse = input("Bonjour sorcier/e. Tu es sur le Chemin de traverse.\n\nSi tu souhaites aller chez Fleury et Bott, librairie de sorcier, tape 1 \nSi tu souhaites aller chez Madame Guipure, magasin de prêt à porter pour mages et sorcier, tape 2 \nSi tu souhaites aller chez Ollivander, fabricant de baguettes magiques, tape 3 \nOù souhaites-tu aller ? ")
     if reponse == '1':
         somme_obligatoire = (0, 60, 63, 231, 899)
-        argent_a_rendre_librairie = int(input("Combien doit-ont vous rendre ? (entrez un entier)"))
+        argent_a_rendre_librairie = int(input("\nVous êtes dans le magasin.\nCombien doit-ont vous rendre ? (entrez un entier)"))
         print("\n")
         chez_fleury_et_bott(argent_a_rendre_librairie)
         print("\n")
-        print("La consigne obligeant, voici l'affichage des sommes à rendre obligatoires:")
-        for elements in somme_obligatoire:
-            print(f"Rendu monnaie pour {elements}€")
-            chez_fleury_et_bott(elements)
-            print("\n")
-    
+        reponse_somme_obligatoire_flury = input("La consigne obligeant, tapez 9 si vous voulez voir l'affichage des sommes à rendre obligatoires. Sinon tapez n'importe quel nombre pour sortir de chez Fleury et Bott. ")
+        if reponse_somme_obligatoire_flury == '9':
+            for elements in somme_obligatoire:
+                print(f"Rendu monnaie pour {elements}€")
+                chez_fleury_et_bott(elements)
+                print("\n")
+        else :
+            reponse = input("Bonjour sorcier/e. Tu es de retour sur le Chemin de traverse.\nSi tu souhaites aller chez Fleury et Bott, librairie de sorcier, tape 1 \nSi tu souhaites aller chez Madame Guipure, magasin de prêt à porter pour mages et sorcier, tape 2 \nSi tu souhaites aller chez Ollivander, fabricant de baguettes magiques, tape 3 \nOù souhaites-tu aller ? ")
+
     elif reponse == '2' :
-        print(Madame_Guipure())
+        Guipur()
     
     #Si on répond n'importe quoi ca fait quand meme ollivander
     elif reponse == '3':
@@ -89,9 +95,34 @@ def menu():
         print("Merci d'entrer un nombre entier, compris entre 1 et 3 et correspondant au bon magasion du chemin de traverse")
         #Moyen de relancer le programme au menu 
 
+def affichage (argent_a_r, dico):
+    '''
+    Affiche le rendu de monnaie détaillé
+    
+    Entrée :
+    Sortie : 
+    '''
+    if argent_a_r == 0 :
+        print("Le commerçant n'a rien à vous rendre")
+    for i in dico.keys():
+            while argent_a_r >= i:
+                dico[i] += 1
+                argent_a_r -= i
+    for i in dico.keys():
+        if dico[i] > 0 :
+            print(f"Le commerçant vous rend {dico[i]} coupure.s de {i} €")
+
 
 def chez_fleury_et_bott(argent_a_rendre):
+    '''
+    Réalise le rendu de monnaie et une partie de l'IHM pour la boutique de fleury et bott
+
+    Entrée : 
+
+    Sortie : 
     
+    
+    '''
     argent_rendu = []
     argent_a_rendre2 = 0
     argent_a_rendre2 = argent_a_rendre
@@ -115,47 +146,18 @@ def chez_fleury_et_bott(argent_a_rendre):
         argent_a_rendre = argent_a_rendre - monnaie[index + 1]
         argent_rendu.append(monnaie[index+1])
     
+    affichage(argent_a_rendre2, dico_monnaie_illimitée)
     
-    if argent_a_rendre2 == 0 :
-        print("Le vendeur n'a rien à vous rendre")
 
-    for i in dico_monnaie_illimitée.keys():
-            while argent_a_rendre2 >= i:
-                dico_monnaie_illimitée[i] += 1
-                argent_a_rendre2 -= i
-    for i in dico_monnaie_illimitée.keys():
-        if dico_monnaie_illimitée[i] > 0 :
-            print(f"Le vendeur vous rend {dico_monnaie_illimitée[i]} coupure.s de {i} €")
-   
-def Madame_Guipure(montant):
-    tiroir_caisse = {
-        200: 1,
-        100: 3,
-        50: 1,
-        20: 1,
-        10: 1,
-        5: 1,
-        2: 5
-    }
-    rendu = {}
-    for billet in sorted(tiroir_caisse.keys(), reverse=True):
-        while montant >= billet and tiroir_caisse[billet] > 0:
-            montant -= billet
-            rendu[billet] = rendu.get(billet, 0) + 1
-            tiroir_caisse[billet] -= 1
-
-    # Afficher le rendu
-    print("Rendu de monnaie :")
-    for billet, quantite in rendu.items():
-        print(f"{quantite} billet(s) de {billet} euro")
-    # Vérifier s'il manque de l'argent à rendre
-    if montant > 0:
-        print(f"Il manque {montant} euro(s) dans le tiroir caisse.")
-
-# Test du programme avec un montant de 325 euros à rendre
-Madame_Guipure(53)
 
 def Ollivander ():
+    '''
+    Fait le change, le calcul et une partie de l'IHM pour le magasin de Ollivander
+    
+    Entrée : 
+    Sortie : 
+    '''
+
     gallions = 0
     mornilles = 0
     noises = 0
@@ -180,40 +182,8 @@ def Ollivander ():
 
 
 
-'''
-def affichage_resultat() :
-    
-'''
-'''
-    Affiche le résulat sous forme de phrase
-
-    Entrée : Un dico 
-    Sortie : Une chaine de caractères
-'''
-'''
-
-    dico_monnaie_illimitée = {\
-    500 : 0 ,
-    200:0 ,
-    100:0 , 
-    50:0 ,
-    20:0 ,
-    10:0 ,
-    5:0  , 
-    2:0   ,
-    1:0 }
-    for i in dico_monnaie_illimitée.keys():
-            while argent >= i:
-                dico_monnaie_illimitée[i] += 1
-                argent -= i
-    for i in dico_monnaie_illimitée.keys():
-        if dico_monnaie_illimitée[i] > 0 :
-            print(f"le vendeur vous rend {dico_monnaie_illimitée[i]} coupure.s de {i}")
-'''
-
-
 """
 PROGRAMME PRINCIPAL
 """
-#Notre programme principal, constitué de fonctions
+
 menu()
