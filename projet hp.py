4b4ebb54f6d9da2b8ce8ea38d18dd757f4a21168
zblou44 <151512087+zblou44@users.noreply.github.com>
2023-12-22 14:54:42 +0100

    Plusieurs modifications avec Valentine
    - On a mis les sommes obligatoires sur Madame Guipure
    - On a enlevé le menu_relance et on a crée un système avec une boucle pour répéter le menu tant que la réponse n'est pas différente à 1,2,3

--- projet hp.py
+++ projet hp.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 22 11:20:07 2023
+
+@author: LOUBNA.ZBITOU
+"""
+
 """
 Harry Potter se fait la malle
 
@@ -62,19 +69,13 @@ rendre = 0
 
 #définition des fonctions dans l'ordre d'utlilisation
 
-
-def menu_relance() :
-    menu()
-
-
-
-
 def affichage (argent_a_r, dico):
     '''
     Affiche le rendu de monnaie détaillé
     
     Entrée :
     Sortie : 
+    
     '''
     if argent_a_r == 0 :
         print("Le commerçant n'a rien à vous rendre")
@@ -109,7 +110,7 @@ def chez_fleury_et_bott(argent_a_rendre):
         5:0  , 
         2:0   ,
         1:0 }
-    
+
     argent_a_rendre2 = argent_a_rendre
     while argent_a_rendre > 0 :
         monnaie = [500,200,100,50,20,10,5,2,1]
@@ -121,7 +122,8 @@ def chez_fleury_et_bott(argent_a_rendre):
     
     affichage(argent_a_rendre2, dico_monnaie_illimitée)
 
-def madame_guipure(montant):
+def Madame_Guipure(montant):
+    somme_obligatoire_2 = (0, 17, 68, 231, 497, 842)
     tiroir_caisse = {
         200: 1,
         100: 3,
@@ -131,25 +133,22 @@ def madame_guipure(montant):
         5: 1,
         2: 5
     }
-    rendu = {}
-    for billet in sorted(tiroir_caisse.keys(), reverse=True):
-        while montant >= billet and tiroir_caisse[billet] > 0:
-            montant -= billet
-            rendu[billet] = rendu.get(billet, 0) + 1
-            tiroir_caisse[billet] -= 1
-
-    # Afficher le rendu
     print("Rendu de monnaie :")
-    for billet, quantite in rendu.items():
-        print(f"{quantite} coupure(s) de {billet} €")
-    # Vérifier s'il manque de l'argent à rendre
+    for element in range(len(somme_obligatoire_2)):
+        rendu = {}
+        
+        for billet in sorted(tiroir_caisse.keys(), reverse=True):
+            while montant >= billet and tiroir_caisse[billet] > 0:
+                montant -= billet
+                rendu[billet] = rendu.get(billet, 0) + 1
+                tiroir_caisse[billet] -= 1
+
+        for billet, quantite in rendu.items():
+            
+            print(f"{quantite} coupure(s) de {billet} €")
+        # Vérifier s'il manque de l'argent à rendre
     if montant > 0:
         print(f"Il manque {montant} euro(s) dans le tiroir caisse.")
-# Test du programme avec un montant de 325 euros à rendre
-#madame_guipure(53)
-
-
-
 
 def Ollivander ():
     '''
@@ -188,47 +187,37 @@ def menu():
     Entré : 
     Sortie 
     '''
-    reponse = input("Bonjour sorcier/e. Tu es sur le Chemin de traverse.\n\nSi tu souhaites aller chez Fleury et Bott, librairie de sorcier, tape 1 \nSi tu souhaites aller chez Madame Guipure, magasin de prêt à porter pour mages et sorcier, tape 2 \nSi tu souhaites aller chez Ollivander, fabricant de baguettes magiques, tape 3 \nSi tu souhaites quitter le chemin de traverse, tape 4 \nOù souhaites-tu aller ? ")
-    if reponse == '1':
-        somme_obligatoire = (0, 60, 63, 231, 899)
-        argent_a_rendre_librairie = int(input("\nVous êtes dans le magasin.\nCombien doit-ont vous rendre ? (entrez un entier)"))
-        print("\n")
-        chez_fleury_et_bott(argent_a_rendre_librairie)
-        print("\n")
-        reponse_somme_obligatoire_flury = input("La consigne obligeant, tapez 9 si vous voulez voir l'affichage des sommes à rendre obligatoires. Sinon tapez n'importe quel nombre pour sortir de chez Fleury et Bott. ")
-        if reponse_somme_obligatoire_flury == '9':
-            for elements in somme_obligatoire:
-                print(f"Rendu monnaie pour {elements}€")
-                chez_fleury_et_bott(elements)
-                print("\n")
+    reponse = input("Bonjour sorcier/e. Tu es sur le Chemin de traverse.\n\nSi tu souhaites aller chez Fleury et Bott, librairie de sorcier, tape 1 \nSi tu souhaites aller chez Madame Guipure, magasin de prêt à porter pour mages et sorcier, tape 2 \nSi tu souhaites aller chez Ollivander, fabricant de baguettes magiques, tape 3 \nSi tu souhaites quitter le chemin de traverse, tape n'importe quel autre chiffre \nOù souhaites-tu aller ? ")   
+    while reponse=='1' or reponse=='2' or reponse=='3' :  
+        if reponse == '1':
+            somme_obligatoire = (0, 60, 63, 231, 899)
+            argent_a_rendre_librairie = int(input("\nVous êtes dans le magasin.\nCombien doit-ont vous rendre ? (entrez un entier)"))
+            print("\n")
+            chez_fleury_et_bott(argent_a_rendre_librairie)
+            print("\n")
+            reponse_somme_obligatoire_flury = input("La consigne obligeant, tapez 9 si vous voulez voir l'affichage des sommes à rendre obligatoires. Sinon tapez n'importe quel nombre pour sortir de chez Fleury et Bott. ")
+            if reponse_somme_obligatoire_flury == '9':
+                for elements in somme_obligatoire:
+                    print(f"Rendu monnaie pour {elements}€")
+                    chez_fleury_et_bott(elements)
+                    print("\n")
+        elif reponse == '2' :
+            somme_obligatoire_2 = (0, 17, 68, 231, 497, 842)
+            argent_a_rendre_guipure = int(input("\nVous êtes dans le magasin de Mme guipure.\nCombien doit-ont vous rendre ? (entrez un entier)"))
+            print("\n")
+            Madame_Guipure(argent_a_rendre_guipure)
+            print("\n")
+            reponse_somme_obligatoire_guipure = input("La consigne obligeant, tapez 5 si vous voulez voir l'affichage des sommes à rendre obligatoires. Sinon tapez n'importe quel nombre pour sortir de chez Madame Guipure. ")
+            if reponse_somme_obligatoire_guipure == '5':
+                for elements_2 in somme_obligatoire_2:
+                    print(f"Rendu monnaie pour {elements_2}€")
+                    Madame_Guipure(elements_2)
+                    print("\n")
         else :
-            
-            
-            menu_relance()
-    elif reponse == '2' :
-        argent_a_rendre_guipure = int(input("\nVous êtes dans le magasin de Mme guipure.\nCombien doit-ont vous rendre ? (entrez un entier)"))
-        print(madame_guipure(argent_a_rendre_guipure))
-    
- 
-    elif reponse == '3':
-        print("\nVous êtes dans la boutique de Ollivander.")
-        Ollivander()
-        reponse_sortie = input("\nPour quitter la boutique et rejoindre le chemin de traverse, tapez 5. \n")
-        if reponse_sortie == '5':
-            menu_relance()
-    
-    elif reponse == '4':
-        print("\nVous quittez le chemin de traverse, à bientôt !")
-        #Quittez le programme 
-    
-    else :
-        print("\nMerci d'entrer un nombre entier, compris entre 1 et 3 et correspondant au bon magasion du chemin de traverse\n")
-        
-        menu_relance()
-        
-
-
-
+            print("\nVous êtes dans la boutique de Ollivander.")
+            Ollivander()
+        reponse = input("Bonjour sorcier/e. Tu es sur le Chemin de traverse.\n\nSi tu souhaites aller chez Fleury et Bott, librairie de sorcier, tape 1 \nSi tu souhaites aller chez Madame Guipure, magasin de prêt à porter pour mages et sorcier, tape 2 \nSi tu souhaites aller chez Ollivander, fabricant de baguettes magiques, tape 3 \nSi tu souhaites quitter le chemin de traverse,  \nOù souhaites-tu aller ? ")
+    print("\nVous quittez le chemin de traverse, à bientôt !")      
 
 """
 PROGRAMME PRINCIPAL
